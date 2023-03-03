43024d434df42ac6c5f5ee06e04ef821032d75d5
manuel.pereira <manuel.pereira@wearecapicua.com>
2023-03-03 15:53:41 -0400

    end of seccion 14

---
+++ public/js/socketClient.js
@@ -0,0 +1,41 @@
+// HTML references
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMessage = document.querySelector('#txtMessage');
+const btnSend = document.querySelector('#btnSend');
+
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    // console.log('Disconnected');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+});
+
+socket.on('send-message', (payload) => {
+    console.log(payload);
+});
+
+btnSend.addEventListener('click', () => {
+
+    const message = txtMessage.value;
+
+    const payload = {
+        message,
+        id: '123ABC',
+        date: new Date().getTime()
+    }
+    
+    socket.emit('send-message', payload, (id) => {
+        console.log('From server', id);
+    });
+    
+});
